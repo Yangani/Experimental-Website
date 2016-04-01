d822b4ed119a2a866128426d9cf7d12d14ef0497
Festus Kiprop <fyangani@gmail.com>
2016-04-01 14:00:00 +0100

    gulp tasks execution

--- tasks/svg.js
+++ tasks/svg.js
@@ -2,3 +2,9 @@ var pkg = require('../package.json')
 var gulp = require('gulp')
 var svgstore = require('gulp-svgstore')
 
+gulp.task('svg', function () {
+  gulp
+    .src(pkg.folders.src + '/svg/*.svg')
+    .pipe(svgstore())
+    .pipe(gulp.dest(pkg.folders.dist + '/svg'))
+})
