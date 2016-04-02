a8766f043c901a11868aa2fb69c67d140dfc06c7
Festus Kiprop <fyangani@gmail.com>
2016-04-02 14:00:00 +0100

    gulp tasks for images fileexecution

--- tasks/image.js
+++ tasks/image.js
@@ -3,3 +3,10 @@ var gulp = require('gulp')
 var changed = require('gulp-changed')
 var imagemin = require('gulp-imagemin')
 
+gulp.task('img', function () {
+  gulp
+    .src(pkg.folders.src + '/img/**')
+    .pipe(changed(pkg.folders.dist + '/img'))
+    .pipe(imagemin())
+    .pipe(gulp.dest(pkg.folders.dist + '/img'))
+})
