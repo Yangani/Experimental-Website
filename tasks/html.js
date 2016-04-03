c6e932bb9bbf6791c14d83db52d3320a8caa7905
Festus Kiprop <fyangani@gmail.com>
2016-04-03 14:00:00 +0100

    gulp execution of html.js for images

--- tasks/html.js
+++ tasks/html.js
@@ -3,3 +3,10 @@ var gulp = require('gulp')
 var htmlmin = require('gulp-htmlmin')
 var jade = require('gulp-jade')
 
+gulp.task('html', function () {
+  gulp
+    .src(pkg.folders.src + '/html/*.jade')
+    .pipe(jade({ pretty: true }))
+    .pipe(htmlmin({ collapseWhitespace: true, minifyJS: true }))
+    .pipe(gulp.dest(''))
+})
