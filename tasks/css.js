7492f3884050342b3265db08a6d6b1c3245c4417
Festus Kiprop <fyangani@gmail.com>
2016-04-07 14:00:00 +0100

    requiring gulp modules and pkgs

---
+++ tasks/css.js
@@ -0,0 +1,6 @@
+var pkg = require('../package.json')
+var gulp = require('gulp')
+var stylus = require('gulp-stylus')
+var autoPrefixer = require('gulp-autoprefixer')
+var cssNano = require('gulp-cssnano')
+
