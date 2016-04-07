c4f0c7cf202f1a9e1dca8b0ab4c1e9ff3afc9dae
Festus Kiprop <fyangani@gmail.com>
2016-04-07 14:00:00 +0100

    synching browser execution function

--- tasks/sync.js
+++ tasks/sync.js
@@ -2,3 +2,10 @@ var pkg = require('../package.json')
 var gulp = require('gulp')
 var browser = require('browser-sync')
 
+gulp.task('sync', function () {
+  browser.init(pkg.folders.dist + '/**', {
+    server: {
+      baseDir: ''
+    }
+  })
+})
