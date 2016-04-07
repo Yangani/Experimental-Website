313979c1ccc5bb0f43dd07266e462e4e22424b56
Festus Kiprop <fyangani@gmail.com>
2016-04-07 14:00:00 +0100

    gulp task excution for css and autoPrefixer for IE, Safari, Opera and Chrome browser

--- tasks/css.js
+++ tasks/css.js
@@ -4,3 +4,11 @@ var stylus = require('gulp-stylus')
 var autoPrefixer = require('gulp-autoprefixer')
 var cssNano = require('gulp-cssnano')
 
+gulp.task('css', function () {
+  gulp
+    .src(pkg.folders.src + '/css/main.styl')
+    .pipe(stylus({ 'include css': true }))
+    .pipe(autoPrefixer('ie >= 8', 'ff >= 3', 'safari >= 4', 'opera >= 12', 'chrome >= 4'))
+    .pipe(cssNano())
+    .pipe(gulp.dest(pkg.folders.dist + '/css'))
+})
